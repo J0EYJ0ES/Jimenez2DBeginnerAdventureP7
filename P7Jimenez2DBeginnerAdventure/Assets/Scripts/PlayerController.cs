c2d291efd5cc68fe5a06726567792c713c36202d
J0EYJ0ES <garuj10@outlook.com>
2025-11-18 15:27:00 -0800

    2D renderer, player control script
    Added a 2D renderer and added on to my player control script

--- P7Jimenez2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P7Jimenez2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -12,9 +12,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 10;
-
+       
         MoveAction.Enable();
     }
 
@@ -23,7 +21,7 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 0.1f;
+        Vector2 position = (Vector2)transform.position + move * 3f * Time.deltaTime;
         transform.position = position;
     }
 }
